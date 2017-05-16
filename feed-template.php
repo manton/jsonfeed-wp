5743649f913d2fc9986726cd1b1ffc164120f4bf
Manton Reece <manton@me.com>
2017-05-16 10:53:35 -0500

    Added user_comment.

--- feed-template.php
+++ feed-template.php
@@ -23,6 +23,7 @@ endwhile;
 
 $feed_json = array(
 	"version" => "http://jsonfeed.org/version/1",
+	"user_comment" => "This feed allows you to read the posts from this site in any feed reader that supports the JSON Feed format. To add this feed to your reader, copy the following URL -- " . get_feed_link("json") . " -- and add it your reader.",
 	"home_page_url" => get_home_url(),
 	"feed_url" => get_feed_link("json"),
 	"title" => get_bloginfo("name"),
