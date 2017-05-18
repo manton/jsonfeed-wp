c4d19ff0600ca40771382a3d5017e4c7aaafa8d7
Kaspars Dambis <hi@kaspars.net>
2017-05-18 13:14:34 +0300

    Format the JSON feed link

--- jsonfeed-wp.php
+++ jsonfeed-wp.php
@@ -14,11 +14,6 @@ License:      MIT
 add_action('init', 'setup_feed_rewrite');
 add_filter('feed_content_type', 'json_feed_content_type', 10, 2);
 
-// I don't know if there is a cleaner way to add this. There is a hook
-// feed_links_show_posts_feed, but that only controls where the main RSS
-// feed is shown or not.
-add_action('wp_head', 'json_feed_link');
-
 function setup_feed_rewrite()
 {
     // Register our function as the feed generator for the /feed/json URL
@@ -41,9 +36,10 @@ function generateJSONFeed()
 	load_template( dirname( __FILE__ ) . '/feed-template.php' );
 }
 
-function json_feed_link()
-{
-   echo '<link rel="alternate" type="application/json" title="JSON Feed" href="' . esc_url( get_feed_link('json') ) . "\" />\n";
-
-   return false;
+add_action( 'wp_head', 'json_feed_link' );
+function json_feed_link() {
+	printf(
+		'<link rel="alternate" type="application/json" title="JSON Feed" href="%s" />',
+		esc_url( get_feed_link( 'json' ) )
+	);
 }
