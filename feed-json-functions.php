4a9ebd4f0b1099f865c129688e1498dd202bf0d5
David Shanske <david@shanske.com>
2020-01-20 20:23:30 +0000

    Fix extra nested array

--- feed-json-functions.php
+++ feed-json-functions.php
@@ -1,10 +1,11 @@
 <?php
 
 
-// Reproduction of self_link with a return and without URL escaping
-function get_json_self_link() {
-	$host = wp_parse_url( home_url() );
-	return apply_filters( 'self_link', set_url_scheme( 'http://' . $host['host'] . wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
+if ( ! function_exists( 'get_self_link' ) ) {
+	function get_self_link() {
+		$host = @parse_url( home_url() );
+		return set_url_scheme( 'http://' . $host['host'] . wp_unslash( $_SERVER['REQUEST_URI'] ) );
+	}
 }
 
 function get_attachment_json_info() {
@@ -52,9 +53,10 @@ function get_link_from_json_feed( $link ) {
 function get_json_feed_data() {
 	$return = array(
 		'version'       => 'https://jsonfeed.org/version/1',
-		'user_comment'  => sprintf( __( 'This feed allows you to read the posts from this site in any feed reader that supports the JSON Feed format. To add this feed to your reader, copy the following URL -- %1$s -- and add it your reader.', 'jsonfeed' ), get_json_self_link() ),
-		'home_page_url' => get_link_from_json_feed( get_json_self_link() ),
-		'feed_url'      => get_json_self_link(),
+		// translators: 1. get_self_link URL
+		'user_comment'  => sprintf( __( 'This feed allows you to read the posts from this site in any feed reader that supports the JSON Feed format. To add this feed to your reader, copy the following URL -- %1$s -- and add it your reader.', 'jsonfeed' ), get_self_link() ),
+		'home_page_url' => get_link_from_json_feed( get_self_link() ),
+		'feed_url'      => get_self_link(),
 		'title'         => get_bloginfo( 'name' ),
 		'description'   => get_bloginfo( 'description' ),
 		'icon'          => get_site_icon_url(),
@@ -131,9 +133,7 @@ function get_json_feed_item() {
 
 	$attachment = get_attachment_json_info();
 	if ( ! empty( $attachment ) ) {
-		$feed_item['attachments'] = array(
-			$attachment,
-		);
+		$feed_item['attachments'] = $attachment;
 	}
 
 	return apply_filters( 'json_feed_item', $feed_item, get_post() );
